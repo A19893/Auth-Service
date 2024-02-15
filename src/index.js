3685f33a97246ed2263bdfaba4887096c417ed3b
A19893 <yasharora2678@gmail.com>
2024-02-15 15:53:11 +0530

    Added creation-token and verification-token method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
+const { UserService } = require("./services");
 const setupAndStartServer = async () => {
   const app = express();
   app.use(express.json({ limit: "50mb", extended: true }));
@@ -10,6 +11,12 @@ const setupAndStartServer = async () => {
   app.listen(PORT, async function () {
     console.clear();
     console.log(`Server listening on port ${PORT}`);
+    const service = new UserService();
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Inlhc2gxMkBnbWFpbC5jb20iLCJpZCI6MiwiaWF0IjoxNzA3OTkyMjcxLCJleHAiOjE3MDc5OTU4NzF9.6huXl_XVAuAtQ2ee-iBCTjntvXklv6ifc9_5ekf3Eu8"
+    const response = service.verifyToken(token)
+    console.log(response)
+    // const token = service.createToken({email : "yash12@gmail.com", id:2});
+    // console.log(token)
   });
 };
 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,5 @@
 const { UserRepository } = require("../repositories");
-
+const jwt = require("jsonwebtoken")
 class UserService {
     constructor(){
         this.userRepository = new UserRepository();
@@ -14,6 +14,28 @@ class UserService {
             console.log("Something went wrong in the service layer");
             throw {error}
         }
+    }
+
+    createToken(user){
+       try{
+         const token = jwt.sign(user,process.env.AUTH_SECRET_KEY,{expiresIn: '1h'});
+         return token;
+       }
+       catch(error){
+        console.log("Something went wrong in the token creation");
+        throw {error}
+       }
+    }
+
+    verifyToken(token){
+        try{
+            const response = jwt.verify(token, process.env.AUTH_SECRET_KEY)
+            return response;
+          }
+          catch(error){
+           console.log("Something went wrong in the token verification");
+           throw {error}
+          }
     }
 }
 
